d0bbf885dc4b8894ca44bd23b20a62d9ef999a16
shoya0091 <syou0091@yahoo.co.jp>
2021-01-29 22:42:14 +0900

    add migration file and model

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 2021_01_19_114915) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "image"
+    t.integer "address_id"
+    t.string "explanation"
     t.string "name", null: false
     t.integer "age", null: false
     t.string "email", default: "", null: false
