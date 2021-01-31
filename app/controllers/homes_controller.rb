2ac2b987631d93eeda5a14d87e15f061c3d66cbe
shoya0091 <syou0091@yahoo.co.jp>
2021-01-31 22:30:03 +0900

    add-user-image

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,29 +6,12 @@ class HomesController < ApplicationController
   end
 
     def new
-      @home = Home.new
-    end
-
-    def create
-      @home = Home.new(home_params)
-      if @home.save
-        HomeMailer.home_mail(@home).deliver
-        redirect_to root_path
-      else
-        render :new
-      end
     end
 
     def inquiry
     end
 
     def PrivacyPolicy
-    end
-
-    private
-
-    def home_params
-      params.require(:home).permit(:name, :text)
     end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user
 
-  def show
+def show
+end
+
+def eidt
+  if @user.id = current_user
+  else
+    render users_path
+  end
+end
+
+def update
+  if @user.update(user_params)
+    redirect_to "/users/#{current_user.id}",notice: 'ユーザー情報を更新しました'
+  else
+    render :edit
+  end
+end
+
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :image, :address_id,:explanation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
