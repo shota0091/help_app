1a2d60377725330d63d7c4fecfec4d5f1a18e386
shoya0091 <syou0091@yahoo.co.jp>
2021-02-07 21:48:20 +0900

    fix-Evaluate-form

--- app/controllers/evaluates_controller.rb
+++ app/controllers/evaluates_controller.rb
@@ -1,2 +1,19 @@
 class EvaluatesController < ApplicationController
+  def new
+    @evaluate = Evaluate.new
+  end
+  
+  def create
+    @evaluate = Evaluate.new(evaluates_params)
+    if @evaluate.save
+      redirect_to "/"
+    else
+      render :show
+    end
+  end
+
+  private
+  def evaluates_params
+    params.require(:evaluate).permit(:speedy, :kindness, :frantically).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
       get 'policy'
     end
   end
-  resources :users
-  resources :evaluates, only: [:create]
+  resources :users 
+  resources :evaluates, only: [:create,:new]
   resources :tags, only: :show
   resources :posts do
     resources :comments, only: [:create,:destroy]
